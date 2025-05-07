259481a432de268646545e1ed250b52e1994720d
Bien <bienjulius0@gmail.com>
2025-05-07 09:04:26 +0800

    Add Lizard and Spock

--- src/ActionIcon.jsx
+++ src/ActionIcon.jsx
@@ -1,12 +1,16 @@
 import { FaRegHandRock } from "react-icons/fa";
 import { FaRegHandPaper } from "react-icons/fa";
 import { FaRegHandScissors } from "react-icons/fa";
+import { FaRegHandLizard } from "react-icons/fa";
+import { FaRegHandSpock } from "react-icons/fa";
 
 function ActionIcon({action, ...props}){
     const icons = {
         rock: FaRegHandRock,
         paper: FaRegHandPaper,
-        scissor: FaRegHandScissors
+        scissors: FaRegHandScissors,
+        lizard: FaRegHandLizard,
+        spock: FaRegHandSpock,
     };
 
     const Icon = icons[action]
